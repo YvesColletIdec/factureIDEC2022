b2f7cd29833d7c6f185d3044396030c959f9510b
YvesColletIdec <85213449+YvesColletIdec@users.noreply.github.com>
2022-05-02 21:15:24 +0200

    ajout windowArticle

---
+++ SolutionFacture/WpfFacture/ArticleViewModel.cs
@@ -0,0 +1,52 @@
+﻿using prjEFCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfFacture
+{
+    //mon viewmodel pour la gestion de  l'interface graphique
+    class ArticleViewModel
+    {
+        private List<Article> _listArticles;
+
+        public List<Article> ListArticles
+        {
+            get { return _listArticles; }
+            set { _listArticles = value; }
+        }
+
+        private Article _articleSelectionne;
+
+        public Article ArticleSelectionne
+        {
+            get { return _articleSelectionne; }
+            set { 
+                _articleSelectionne = value;
+                Console.WriteLine(_articleSelectionne.Designation);
+            }
+        }
+
+        private List<Categorie> _listCategorie;
+
+        public List<Categorie> ListCategorie
+        {
+            get { return _listCategorie; }
+            set { _listCategorie = value; }
+        }
+
+
+
+        //constructeur pour récupérer toutes les données
+        public ArticleViewModel()
+        {
+            //récupération des articles
+            FactureContext context = new FactureContext();
+            ListArticles = context.Articles.ToList();
+            ListCategorie = context.Categories.ToList();
+        }
+
+    }
+}
---
+++ SolutionFacture/WpfFacture/WindowArticle.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfFacture
+{
+    /// <summary>
+    /// Logique d'interaction pour WindowArticle.xaml
+    /// </summary>
+    public partial class WindowArticle : Window
+    {
+        public WindowArticle()
+        {
+            InitializeComponent();
+            //instaciation d'un viewmodel pour faire la relation entre les composants graphiques
+            //et les propriétés du viewmodel
+            DataContext = new ArticleViewModel();
+        }
+    }
+}
